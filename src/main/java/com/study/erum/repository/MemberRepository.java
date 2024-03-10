d019631e7cfbba26eee526c94db41112f1b93449
leehana <lubihana12@gmail.com>
2024-03-11 02:16:39 +0900

    MemberRepository.java -> save메소드 생성(값이 넘어오는지 확인후 MyBatis로 전환 예정

--- src/main/java/com/study/erum/repository/MemberRepository.java
+++ src/main/java/com/study/erum/repository/MemberRepository.java
@@ -2,10 +2,17 @@ package com.study.erum.repository;
 
 import org.springframework.stereotype.Repository;
 
+import com.study.erum.dto.MemberDTO;
+
 import lombok.RequiredArgsConstructor;
 
 @Repository
 @RequiredArgsConstructor
 public class MemberRepository {
+  
+  public static int save(MemberDTO memberDTO) {
+    System.out.println("memberDTO = " + memberDTO);
+    return 0;
+  }
  
 }
