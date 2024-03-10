41363116389b66ff7bec71f3abdea768ef5df062
leehana <lubihana12@gmail.com>
2024-03-11 01:10:45 +0900

    MemberDTO.java 코드작성 완료(회원정봅를 담고있는 개체

---
+++ src/main/java/com/study/erum/dto/MemberDTO.java
@@ -0,0 +1,18 @@
+package com.study.erum.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+//(Data Transfer Object)-데이터 전송 객체. 비즈니스 로직과 데이터베이스 사이에서 데이터를 전송하는 데 사용,요청데이터를 담고있는객체
+public class MemberDTO {
+  private Long id; // Long 타입은 64비트(=8바이트) 정수 
+  private String memberEmail;
+  private String memberPassword;
+  private String memberName;
+  private int memberAge; // int 타입은 32비트(4바이트) 정수.
+  private String memberMobile;
+}
