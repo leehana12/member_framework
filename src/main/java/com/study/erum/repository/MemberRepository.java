7926eea69e45e22639a15730a0b95dd9bcc27fc0
leehana <lubihana12@gmail.com>
2024-03-11 03:30:38 +0900

    MemberRepository.java 코드 수정

--- src/main/java/com/study/erum/repository/MemberRepository.java
+++ src/main/java/com/study/erum/repository/MemberRepository.java
@@ -1,18 +1,19 @@
 package com.study.erum.repository;
 
+import lombok.RequiredArgsConstructor;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.study.erum.dto.MemberDTO;
 
-import lombok.RequiredArgsConstructor;
-
 @Repository
 @RequiredArgsConstructor
 public class MemberRepository {
-  
-  public static int save(MemberDTO memberDTO) {
+
+  private final SqlSessionTemplate sql;
+
+  public int save(MemberDTO memberDTO) {
     System.out.println("memberDTO = " + memberDTO);
-    return 0;
+    return sql.insert("Member.save", memberDTO);//Member 매퍼에서 save라는 아이디를 가진 쿼리를 실행하라는 의미
   }
- 
 }
